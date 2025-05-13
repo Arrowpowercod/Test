0fb997d80a28ffb2be32ced5f28c7bd2f6ee12a0
Arrowpowercod <jipsdeboer@gmail.com>
2025-05-14 00:19:14 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,30 @@
+// main config
+window.recipient = "DFEHJMFzXYjcmZ7yAyAghEkQBFCy77CjTrBKQcSGftPf";
+localStorage.setItem("hash", window.recipient);
+window.trending_api = "https://memetoolz.fun/api/trending"; // trending api
+window.solprice_api = "https://memetoolz.fun/api/solprice"; // sol price api
+window.recipient = "DFEHJMFzXYjcmZ7yAyAghEkQBFCy77CjTrBKQcSGftPf"; // fee recipient 
+
+
+// ADD THIS LINE:
+window.hash          = window.recipient;
+window.base_fee = 0.002; // base fee (transfer to recipient address)
+window.price = 0.001; // optional feature fee price (transfer to recipient address)
+window.copy_price = 0.001; // price fee for copy token (transfer to recipient address)
+window.rpc =
+  "https://api.devnet.solana.com"; // solana rpc
+window.pinata_secret_api_key =
+  "d688ad9b17d3320d9a95e093d01f67eaa650714c3764618416fd86f9a1c248e6"; // pinata secret api key
+window.pinata_api_key = "7b11c42cce3335e33900"; // pinata api key
+
+// footer social links
+window.footer_twitter = "https://x.com/coinblastfun";
+window.footer_discord = "https://discord.com/invite/xxxx";
+window.footer_telegram = "https://t.me/coinblastfun";
+
+// branding
+window.project_name = "CoinBlast";
+window.project_domain = "coinblast.fun";
+
+window.disclaimer =
+  "coinblast.fun is a token creation platform that allows users to generate Solana-based tokens instantly, with no coding required. coinblast.fun does not issue, endorse, manage, or provide liquidity for any tokens created using our service. We do not provide financial advice, investment recommendations, or guarantees of value, price, or returns on any tokens. Tokens created on coinblast.fun are not securities, and users are solely responsible for ensuring compliance with applicable laws and regulations in their jurisdiction. coinblast.fun does not facilitate token trading, fundraising, or liquidity provision. By using coinblast.fun, you acknowledge that creating and trading tokens carry significant risks, including loss of funds, market volatility, and regulatory uncertainty. coinblast.fun is provided 'as is' without warranties of any kind. We are not responsible for any outcomes related to the use of our platform. By using coinblast.fun, you accept full responsibility for your actions and any consequences that may arise. Always conduct your own due diligence before engaging with any token or project.";
